b6b3f920c906be709d9d56718d26aec027f46145
PI VIRGIN <76819375+SF1960@users.noreply.github.com>
2023-05-31 15:28:10 +0100

    Version 1.1
    Added DEBUG definition which replaces Serial.print() with debug_print()
    To toggle this on and off use DEBUG = 1 to DEBUG = 0
    By using #defibe DEBUG = 0 we prevent the compiling of Serial.print commands and save memory
    Changed all the Serial.print() commands for debug_print()

--- ESP32_NOW_RECEIVE_SOCKETS.ino
+++ ESP32_NOW_RECEIVE_SOCKETS.ino
@@ -9,7 +9,16 @@
 * Local:        C:\Documents\Github\remote_socket_control
 * Comments:     This is the main code to receive to the remote sockets. This revision 
 * drives are relay and uses the NO contact to check that the relay has engaged
-****************************************************************************************
+***************************************************************************************/
+
+/***************************************************************************************
+* Revision History
+*
+* 1.0 Original working revision with 1 x LED and 2 x Relay outputs controlled by Arduino IOT
+*
+* 1.1 Added DEBUG definition to remove compiled Serial.print commands
+*
+****************************************************************************************/
 
 /***************************************************************************************
   ESP-NOW Demo - Receive The RESPONDER
@@ -26,6 +35,21 @@
 
 *****************************************************************************************/
 
+/*******************************************************************************************************
+* debug_print() text that replaces Serial.Print() and therefore does not get compiled, saving memory!
+* Set to DEBUG to 1 to print to the serial monitor. When you are happy with the code set DEBUG to 0
+* Thanks to Ralph S Bacon https://youtu.be/--KxxMaiwSE
+********************************************************************************************************/
+#define DEBUG 0 
+
+#if DEBUG == 1
+  #define debug_print(x)   Serial.print(x)
+  #define debug_println(x) Serial.println(x)
+#else
+  #define debug_print(x)
+  #define debug_println(x)
+#endif
+
 // Include Libraries
 #include <esp_now.h>
 #include <WiFi.h>
@@ -41,7 +65,7 @@
 // broadcastAddress_3[] = {0x7C, 0x87, 0xCE, 0x30, 0x91, 0xCC}; //#16 socket 3
 
 /*******************************************************************************************************
-*    CHANGE THE SOCKET NUMBER AND UPLOAD TO EACH UNIQUE CONTROLLER                                     */
+*    CHANGE THE SOCKET NUMBER (1, 2, 3) AND UPLOAD TO EACH UNIQUE CONTROLLER                           */
 const int THIS_SOCKET = 2;     
 /*******************************************************************************************************/
 
@@ -53,7 +77,7 @@ bool ledStatus = false;
 // In the future this will just include POWER_ON and POWER_OFF boolean commands
 typedef struct socket_message {
   bool socket_state;            // On or Off
-  int socket_number;            // which socket 1 to 3
+  int socket_number;            // which socket (1 to 3)
 } socket_message;
 
 // Create an object called myData 
@@ -66,11 +90,11 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
 
   if (myData.socket_number == THIS_SOCKET){ // if the socket number matches then perform the code
-    Serial.println();
-    Serial.print("Socket ID      : ");  Serial.println(myData.socket_number);
-    Serial.print("Bytes received : ");  Serial.println(len);
-    Serial.print("Boolean Value  : ");  Serial.println(myData.socket_state);
-    Serial.println();
+    debug_println();
+    debug_print("Socket ID      : ");  debug_println(myData.socket_number);
+    debug_print("Bytes received : ");  debug_println(len);
+    debug_print("Boolean Value  : ");  debug_println(myData.socket_state);
+    debug_println();
     
     ledStatus = myData.socket_state;
     if (ledStatus){
@@ -84,8 +108,8 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
     }
 
   }else {
-    Serial.print("Not for socket : ");
-    Serial.println(myData.socket_number);
+    debug_print("Not for socket : ");
+    debug_println(myData.socket_number);
   }
 
 }
@@ -100,12 +124,12 @@ void setup() {
 
   // Initialize ESP-NOW
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
+    debug_println("Error initializing ESP-NOW");
     return;
   } else {
-      Serial.println("ESP-NOW initialised");
-      Serial.print("WiFi Channel: ");
-      Serial.println(WiFi.channel());
+      debug_println("ESP-NOW initialised");
+      debug_print("WiFi Channel: ");
+      debug_println(WiFi.channel());
   }
 
   // Register the ESP32 NOW callback function to a sketch function
@@ -119,7 +143,7 @@ void setup() {
   digitalWrite(RELAY_1, HIGH); // TURN OFF RELAY 1
   digitalWrite(RELAY_2, HIGH); // TURN OFF RELAY 2
 
-  Serial.println("Setup complete");
+  debug_println("Setup complete");
 }
 
 // No code needed for this basic sketch
